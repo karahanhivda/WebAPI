f2f5ad7267ef5ce3bcdedd1436bee995b9c2828b
karahanhivda <hivda.krhn@gmail.com>
2024-05-28 14:49:26 +0300

    Crud işlemler eklendi.

--- WebAPIGirisss/Controllers/ArabaController.cs
+++ WebAPIGirisss/Controllers/ArabaController.cs
@@ -13,7 +13,7 @@ namespace WebAPIGirisss.Controllers
         private AppDbContext _appDbContext;
         public ArabaController(AppDbContext context)
         {
-            context = _appDbContext;
+            _appDbContext = context;
         }
 
         [HttpGet("Arabalar")]
@@ -21,6 +21,54 @@ namespace WebAPIGirisss.Controllers
         {
 
             return _appDbContext.Arabalar.ToList();
+        }
+
+        [HttpPost("Arabalar")]
+        public IActionResult ArabaEkle([FromBody] Araba Araba)
+        {
+            Araba araba = new Araba();
+            araba.Marka=Araba.Marka;
+            araba.Renk=Araba.Renk;
+
+            _appDbContext.Arabalar.Add(araba);
+            _appDbContext.SaveChanges();
+
+            return Ok("Eklendi!");   
+        }
+
+        [HttpDelete("Arabalar")]
+        public IActionResult ArabaSil(int Id) 
+        {
+            //_appDbContext.Arabalar.FirstOrDefault(a => a.Id==Id);
+            var sil = _appDbContext.Arabalar.FirstOrDefault(a => a.Id == Id);
+            if (sil == null)
+            {
+                return BadRequest("Hata");
+            }
+             
+            _appDbContext.Arabalar.Remove(sil);
+            _appDbContext.SaveChanges();
+
+            return Ok("Silindi");    
+        }
+
+        [HttpPut("Arabalar")]
+        public IActionResult ArabaGuncelle(int Id, Araba araba)
+        {
+            Araba arabaa = new Araba();
+            var guncelle = _appDbContext.Arabalar.FirstOrDefault(arabaa => arabaa.Id == Id);
+        
+            if(guncelle == null)
+            {
+                return BadRequest("Hata!");
+            }
+            guncelle.Marka = araba.Marka;
+            guncelle.Renk = araba.Renk;
+
+            _appDbContext.Arabalar.Update(guncelle);
+            _appDbContext.SaveChanges();
+
+            return Ok();
         }
     }
 }
